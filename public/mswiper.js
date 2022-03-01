b973751edf9fd641c2ac9e872f791faead597572
aLikan-jpg <alikinnikita2002@gmail.com>
2022-03-01 17:15:54 +0300

    Frontend add

---
+++ public/mswiper.js
@@ -0,0 +1,24 @@
+new Swiper('.swiper',{
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    speed: 400,
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true
+    // },
+    effect: 'cube',
+    cubeEffect: {
+        slideShadows: true,
+        shadow: true,
+        shadowOffset: 20,
+    },
+    // autoplay:{
+    //     delay: 1000,
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: false
+    // },
+
+
+});
